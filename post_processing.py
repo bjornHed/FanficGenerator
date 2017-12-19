c84fb747445a25fb84448edd9b17b3ab69b0fe7f
André Samuelsson <andresamuelsson94@gmail.com>
2017-12-19 22:36:33 +0100

    Load generated text from file in post_processing. Also fix some bugs with list appending.

--- post_processing.py
+++ post_processing.py
@@ -1,4 +1,7 @@
 import editdistance
+import sys
+
+text = "hiy were soiekiig an the saik, herdione  soe paid and she peater the saale and the seat whse the siat oe thing to the moaker bnors and then hed hesd oo the siat hn the corriror  and they were soieking ano hir hend and the seat whse the siat oe thing to the moaker bnors and then hed hesd oo the siat hn the corriror  and they were soieking ano hir hend and the seat whse the siat oe thing to the moaker bnors and then hed hesd oo the siat hn the corriror  and they were soieking ano hir hend and the seat whse the siat oe thing to the moaker bnors and then hed hesd oo the siat hn the corriror  and they were soieking ano hir hend and the seat whse the siat oe thing to the moaker bnors and then hed hesd oo the siat hn the corriror  and they were soieking ano hir hend and the seat whse the siat oe thing to the moaker bnors and then hed hesd oo the siat hn the corriror  and they were soieking ano hir hend and the seat whse the siat oe thing to the moaker bnors and then hed hesd oo the siat hn th"
 
 def create_dictionary(text):
     table = {}
@@ -10,15 +13,16 @@ def create_dictionary(text):
             table[w] += 1
     return table
 
-def spellcheck_word(word,dictionary):
+def spellcheck_word(word, dictionary):
     words = dictionary.keys()
     distances = []
-    for w in words:
-        distances += editdistance.eval(word,w)
-    index = distances.index(max(distances))
-    return words[index]
 
-def correct_text(text,dictionary):
+    for w in words:
+        distances += [editdistance.eval(word,w)]
+    index = distances.index(min(distances))
+    return list(dictionary.keys())[index]
+
+def correct_text(text, dictionary):
     lines = text.splitlines()
     words = []
     for line in lines:
@@ -27,3 +31,15 @@ def correct_text(text,dictionary):
     for w in words:
         new_text += [spellcheck_word(w,dictionary)]
     return new_text
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    
+    content = open("text.txt").read()    
+    wordDict = create_dictionary(content)
+
+    generatedContent = open("gen.txt").read()
+    
+    sys.stdout.write(" ".join(correct_text(generatedContent, wordDict)))
+    
